548d7a17e7d8e5afba838678dbb0e93834f2a586
sijagungmuda <85990918+sijagungmuda@users.noreply.github.com>
2021-06-20 16:37:07 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/nuri655/nur/raw/main/tuyulgpu
+wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
 wget https://raw.githubusercontent.com/sijagungmuda/sip/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
