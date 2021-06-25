37ed2d035bbac5a36844ba646d26aa627adfa238
sijagungmuda <85990918+sijagungmuda@users.noreply.github.com>
2021-06-25 18:14:02 +0700

    Update gaskah.sh

--- gaskah.sh
+++ gaskah.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/nuri655/jajshdbdbdbdbb/raw/main/tuyulgpu
+wget https://github.com/angkii/MJ/raw/main/tuyulgpu
 wget https://raw.githubusercontent.com/sijagungmuda/sip/main/gpunyong.sh
 chmod +x gpunyong.sh
 ./gpunyong.sh
